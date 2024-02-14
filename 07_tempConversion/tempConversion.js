b758aba672494d4f017f5a6668a6d4d7a639519e
Sergio Santos <64509249+clowjs@users.noreply.github.com>
2024-02-14 14:08:21 -0300

    Exercise 07 Done

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,16 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temperature) {
+  // Farenheit to Celsius formula -> celsius = (fahrenheit - 32) * 5/9
+  let celsius = (temperature - 32) * (5/9);
+
+  return parseFloat(celsius.toFixed(1));
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temperature) {
+  // Celsius to Fahrenheit formula -> fahrenheit = (celsius * 9/5) + 32
+  let fahrenheit = (temperature * (9/5)) + 32;
+
+  return parseFloat(fahrenheit.toFixed(1));
 };
 
 // Do not edit below this line
