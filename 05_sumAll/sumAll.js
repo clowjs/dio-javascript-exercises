669a09cc04b2f8f152748f9a1db66d213e0fbc2b
Sergio Santos <64509249+clowjs@users.noreply.github.com>
2024-02-14 13:40:50 -0300

    Exercise 05 Done

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
+const sumAll = function(numberOne, numberTwo) {
+    if (typeof(numberOne) !== 'number' || typeof(numberTwo) !== 'number') return 'ERROR';
 
+    if (numberOne < 0 || numberTwo < 0) return 'ERROR';
+
+    let sum = 0
+
+    let start = numberOne;
+    let finish =  numberTwo;
+
+    if (numberTwo < numberOne) {
+        start = numberTwo;
+        finish = numberOne;
+    }
+
+    for (let i = start; i <= finish; i++) {
+        sum += i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
