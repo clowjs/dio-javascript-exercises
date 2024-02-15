75cfb25ad82e55032563c8d74037db586e15f505
Sergio Santos <64509249+clowjs@users.noreply.github.com>
2024-02-15 12:26:57 -0300

    cleanup

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -22,17 +22,3 @@ const fibonacci = function(num) {
 
 // Do not edit below this line
 module.exports = fibonacci;
-
-
-// # Exercise 10 - Fibonacci
-
-// Create a function that returns a specific member of the Fibonacci sequence:
-
-// > A series of numbers in which each number ( Fibonacci number ) is the sum of the two preceding numbers.
-// > In this exercise, the Fibonacci sequence used is 1, 1, 2, 3, 5, 8, etc.
-// > To learn more about Fibonacci sequences, go to: https://en.wikipedia.org/wiki/Fibonacci_sequence
-
-// ```javascript
-// fibonacci(4); // returns the 4th member of the series: 3  (1, 1, 2, 3)
-// fibonacci(6); // returns 8
-// ```
