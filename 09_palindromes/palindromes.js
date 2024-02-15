795285a2f462e1aad6e9177d715aa18ad614bcd1
Sergio Santos <64509249+clowjs@users.noreply.github.com>
2024-02-15 11:39:09 -0300

    Exercise 09 Done

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,18 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    string = string.toLowerCase();
 
+    let stringArray = string.split('');
+    stringArray = stringArray.filter(c => /^[a-zA-Z0-9]+$/.test(c))
+    let reversedStringArray = stringArray.toReversed();
+
+    for (let i = 0; i < stringArray.length; i++) {
+        if (stringArray[i] !== reversedStringArray[i]) return false;
+    }
+
+    return true;
 };
+
+palindromes('test te!');
 
 // Do not edit below this line
 module.exports = palindromes;
