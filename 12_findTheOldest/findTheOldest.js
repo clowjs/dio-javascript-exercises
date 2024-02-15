547f054eeb745556ad3aa1971d0ddbeb6d7fc06a
Sergio Santos <64509249+clowjs@users.noreply.github.com>
2024-02-15 12:25:15 -0300

    Exercise 12 Done

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,28 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    let person = people[0];
 
+    people.forEach(p => {
+        let oldestAge = 0;
+        let personAge = 0;
+
+        if ("yearOfDeath" in person) {
+            oldestAge = person.yearOfDeath - person.yearOfBirth;
+        } else {
+            oldestAge = (new Date().getFullYear()) - person.yearOfBirth;
+        }
+
+        if ("yearOfDeath" in p) {
+            personAge = p.yearOfDeath - p.yearOfBirth;
+        } else {
+            personAge = (new Date().getFullYear()) - p.yearOfBirth;
+        }
+
+        if (personAge > oldestAge) {
+            person = p;
+        }
+    });
+
+    return person;
 };
 
 // Do not edit below this line
